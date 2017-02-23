3df8eb6beeb67fcade6e77c3e6a6b56284aa9bad
DESKTOP-J8M7AQJ\alex <dp.prk.kiselov@gmail.com>
2017-02-23 04:08:19 +0200

    Написать программу, в которой к таблице Customers, которую мы постоянно использовали на занятиях(Если нет, тогда создайте id, name, account и заполните ) необходимо реализовать:
    1) Выборку всех клиентов, у которых 1я и последняя буквы такие же, как указанные пользователем.
    2) Выборку клиентов, у которых значение account меньше среднего арифметического значения столбца account всех клиентов.
    3) Обновление account всех клиентов, у которых значение столбца account равно указанному пользователем на другое указанное пользователем значение.
    4) Удаление всех пользователей, у которых значение account содержит числа 1, 3, 6 и 9.
    
    Результат запроса 1 сохранять в XElement, потом в файл 1.xml. Формат xml файла:
    <Query1>
     <Custmomer1>
      <Name>Вася</Name>
      <Account>2343</Account>
     </Customer1>
     <Custmomer2>
      <Name>Вася</Name>
      <Account>2343</Account>
     </Customer2>
    </Query1>
    Запросы к таблице Customers реализовать с помощью LINQ.
    Обязательно: обработка исключений, проверки, документация на английском языке, LINQ.

---
+++ Kiselov_HW_WpfCustomersLinq/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace Kiselov_HW_WpfCustomersLinq
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataClassesCustomersDataContext _context = new DataClassesCustomersDataContext();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Method starts searching of elements with equal first and last letters in client's name
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonLetterSearch_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(TextBoxFirstLetter.Text) || string.IsNullOrEmpty(TextBoxFirstLetter.Text))
+                {
+                    throw new Exception("Enter the correct first letter");
+                }
+                if (string.IsNullOrEmpty(TextBoxLastLetter.Text) || string.IsNullOrEmpty(TextBoxLastLetter.Text))
+                {
+                    throw new Exception("Enter the correct last letter");
+                }
+                List<Customer> context = (from X in _context.Customers
+                    where X.name.ToLower()[0].Equals(TextBoxFirstLetter.Text.ToLower()[0])
+                          && X.name.ToLower()[X.name.Length - 1].Equals(TextBoxLastLetter.Text.ToLower()[0])
+                    select X).ToList();
+
+                DataGridLetters.DataContext = context;
+
+                DataSourceToXElement(context);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK);
+            }
+        }
+
+        /// <summary>
+        /// Method returns all clients, who have salary which is smaller than average 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonAverage_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataGridAverage.DataContext = from X in _context.Customers
+                    where X.account < (from Y in _context.Customers select Y.account).Average()
+                    select X;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK);
+            }
+        }
+
+        /// <summary>
+        /// Method updates client's accounts
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonAccountSearch_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                (from X in _context.Customers
+                    where X.account == decimal.Parse(TextBoxComparable.Text)
+                    select X).ToList().ForEach(X => X.account = decimal.Parse(TextBoxNewValue.Text));
+
+                _context.SubmitChanges();
+                DataGridAccount.DataContext = _context.Customers;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK);
+            }
+        }
+
+        /// <summary>
+        /// Method deletes clients with direct accounts 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonDigitSearch_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string[] separator = TextBoxDigit.Text.Split(',');
+                var accountToDelete = new List<Customer>();
+
+                foreach (var digit in separator)
+                {
+                    accountToDelete.AddRange(
+                        from X in _context.Customers
+                        where X.account.ToString().Contains(digit)
+                        select X
+                        );
+                }
+
+                _context.Customers.DeleteAllOnSubmit(accountToDelete);
+                _context.SubmitChanges();
+                DataGridDigit.DataContext = _context.Customers;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK);
+            }
+        }
+
+        /// <summary>
+        /// Method converts existing database into XML document
+        /// </summary>
+        /// <param name="listOfCustomers"></param>
+        private void DataSourceToXElement(List<Customer> listOfCustomers)
+        {
+            XElement root = new XElement("Customers");
+            try
+            {
+                for (int i = 0; i < listOfCustomers.Count; i++)
+                {
+                    root.Add(new XElement(string.Format("Customer" + listOfCustomers[i].Id.ToString()),
+                        new XElement("Name", listOfCustomers[i].name)));
+                    root.Element(string.Format("Customer" + listOfCustomers[i].Id.ToString()))
+                        .Add(new XElement("Account", listOfCustomers[i].account));
+                }
+                root.Save("1.xml");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK);
+            }
+        }
+    }
+}
